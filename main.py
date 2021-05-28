f8016cd4519b410ca7ce19694e0bedb624c7baa8
SILU CHEN <SILUCHEN@chensiludeMacBook-Pro.local>
2021-05-28 16:20:32 +0800

    change sqlite db name

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ login_manager.init_app(app)
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"sqlite:///posts.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
